424c5c86d8efdf0a46d6e1a797d9e5b8e7c05559
budimanlai <budiman.lai@gmail.com>
2022-12-01 12:05:00 +0700

    update version

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	srv := services.NewService("config/main.conf")
 	srv.AppName = "WhatsApp Send Message"
-	srv.Version = "0.0.1"
+	srv.Version = "0.0.3"
 
 	cn, e1 := dbm.Connect("mysql", srv.Config.GetString("iam.hostname"), srv.Config.GetInt("iam.port"),
 		srv.Config.GetString("iam.username"), srv.Config.GetString("iam.password"), srv.Config.GetString("iam.database"))
